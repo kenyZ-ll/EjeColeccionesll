e7d2d6422e7022c93d30a320d854193e98a0c87e
KNZZY <cpd.donpeligros@gmail.com>
2026-03-24 13:19:06 +0100

    Creo que lo he echo bien

--- src/ejeHashMap/eje04/Eliza.java
+++ src/ejeHashMap/eje04/Eliza.java
@@ -1,57 +1,64 @@
 package ejeHashMap.eje04;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Eliza {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		var diccionario = new HashMap <String,String>();
+
+		var map = new HashMap<String, String>();
+
+		map.put("HOLA", "Hola, ¿qué tal?");
+		map.put("ENCANTADO", "Encantado de conocerte yo también");
+		map.put("ADIOS", "Adiós, espero volverte a ver pronto");
+		map.put("HORA", "Lo siento, no llevo reloj");
+		map.put("NOMBRE", "Mi nombre es Eliza");
+
+		String msgDefault = "Lo siento, no te comprendo";
+		String msgBloquedo = "No eres muy educado";
+
+		var listapalabraBloquear =  Arrays.asList("caca", "culo", "pedo", "pis");
+
 		Scanner sc = new Scanner(System.in);
-		diccionario.put("HOLA", "Hola qué tal");
-		diccionario.put("ENCANTADO", "Encantado de conocerte, yo tambien");
-		diccionario.put("HORA", "Lo siento, no tengo reloj");
-		diccionario.put("ADIOS", "Adios, espero volver a verte pronto");
-		diccionario.put("NOMBRE", "Mi nombre es Eliza");
-		
-		
-		String msgDefault ="Lo siento, no te comprendo";
-		System.out.println("Bienvenido al sistema ELIZA");
-		
+		System.out.println(" Bienvenido al sistema Eliza 0.0:");
 		String frase;
-		var fin = false;
-		
+		boolean fin = false;
 		do {
-			frase=sc.nextLine();
-	
-			boolean encontrar=false;
-			for(String clave:diccionario.keySet()) {
-				
-				if(frase.toUpperCase().contains(clave)) {
-					encontrar=true;
-					System.out.println(diccionario.get(clave));
-					if(frase.toUpperCase().contains("ADIOS")) {
-						fin=true;
-					}
+			frase = sc.nextLine();
+
+			boolean correcto = true;
+			for (String valor : listapalabraBloquear) {
+				if (frase.contains(valor)) {
+					System.out.println(msgBloquedo + ">" + valor.toUpperCase() + "<");
+					correcto = false;
 					break;
-					
 				}
-				
-				
 			}
-			if (!encontrar) {
-				System.out.println("No te comprendo");
+
+			if (correcto) {
+				boolean encontrada = false;
+
+				for (String clave : map.keySet()) {
+					if (frase.toUpperCase().contains(clave)) {
+						encontrada = true;
+						System.out.println(">" + map.get(clave));
+						if (frase.toUpperCase().contains("ADIOS")) {
+							fin = true;
+						}
+						break;
+					}
+				}
+
+				if (!encontrada) {
+					System.out.println(msgDefault);
+				}
 			}
-			
-		}while (! fin);
-		System.out.println("Gracias por su visita");
-	
-		
-		
+
+		} while (!fin);
+		System.out.println(" Muchas gracias por conectarse");
 	}
+
 }
